33da88ed2e22b99f45d017544ba6eaaf74d2a6d6
Eman <emanhatesschool1234@gmail.com>
2025-01-08 22:56:32 +0800

    main to normal mode button, fix some bugs

--- app/src/main/java/com/eman/comorgquizweek4practice/MainActivity.java
+++ app/src/main/java/com/eman/comorgquizweek4practice/MainActivity.java
@@ -1,6 +1,9 @@
 package com.eman.comorgquizweek4practice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_normal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        button_normal = findViewById(R.id.button_normal);
+        button_normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NormalModeActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/eman/comorgquizweek4practice/NormalModeActivity.java
+++ app/src/main/java/com/eman/comorgquizweek4practice/NormalModeActivity.java
@@ -1,6 +1,8 @@
 package com.eman.comorgquizweek4practice;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileWriter;
+
 public class NormalModeActivity extends AppCompatActivity {
+
+    int questionNumber = 0;
+    TextView textView_questionNumber;
+    TextView textView_question;
+    EditText editText_answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,23 @@ public class NormalModeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        questionNumber = 0;
+        textView_questionNumber = findViewById(R.id.textview_question_number);
+        textView_question = findViewById(R.id.textview_question);
+        editText_answer = findViewById(R.id.edittext_answer);
+        try(FileWriter writer = new FileWriter("test.txt")){
+            writer.write("test");
+        }
+        catch (Exception e) {
+            //
+        }
+    }
+
+    private void importQuestion() {
+
+    }
+
+    private void nextQuestion() {
+
     }
 }
