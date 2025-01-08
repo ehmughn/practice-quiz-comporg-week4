2e420d51f021cd3ef9545a82818c0b13bbebf9fa
Eman <emanhatesschool1234@gmail.com>
2025-01-08 22:33:46 +0800

    remove unnecessary layout_gravity

---
+++ app/src/main/java/com/eman/comorgquizweek4practice/NormalModeActivity.java
@@ -0,0 +1,24 @@
+package com.eman.comorgquizweek4practice;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NormalModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_normal_mode);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
